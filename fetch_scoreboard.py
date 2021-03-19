1dfe6a6246e45d121d3e7a2e29f918b17671f6c6
Mark Norman Francis <norm@201created.com>
2021-03-19 12:57:46 +0000

    Remove extraneous whitespace from HTML text

--- fetch_scoreboard.py
+++ fetch_scoreboard.py
@@ -9,7 +9,7 @@ import toml
 
 
 def text_of(element):
-    return element.get_text().replace('\n', '')
+    return element.get_text().replace('\n', '').strip()
 
 
 USER_AGENT='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
