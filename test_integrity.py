41bf9508b84e04d237e3eacb860192e577c618d6
Mark Norman Francis <norm@201created.com>
2021-03-20 08:55:24 +0000

    Add data for 1968

--- test_integrity.py
+++ test_integrity.py
@@ -260,7 +260,7 @@ class TestScoresIntegrity:
                         assert points in (1, 2, 3, 4, 5)
                     elif year >= 1964 and year <= 1966:
                         assert points in (1, 3, 5, 6, 9)
-                    elif year == 1967:
+                    elif year >= 1967 and year <= 1970:
                         assert points >= 1
                         assert points <= 10
                     else:
@@ -279,7 +279,7 @@ class TestScoresIntegrity:
                     assert total == 15
                 elif year >= 1964 and year <= 1966:
                     assert total == 9
-                elif year == 1967:
+                elif year >= 1967 and year <= 1970:
                     assert total == 10
                 else:
                     raise UnknownYear
